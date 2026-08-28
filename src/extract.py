b1b251ad860ca2993a16e7712b6c8d338557670a
Syed Saad <saadhamdani@Saad-MacBook-Air.local>
2026-08-27 20:46:21 -0700

    Add print statement to extract

--- src/extract.py
+++ src/extract.py
@@ -10,4 +10,5 @@ def extract_data(file_path: str) -> pd.DataFrame:
     """Read a CSV file and return it as a DataFrame."""
     df = pd.read_csv(file_path)
     print(f"[EXTRACT] Loaded yes yes {len(df)} rows from {file_path}")
+    print(f"helllo")
     return df
